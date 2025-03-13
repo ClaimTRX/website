bbd36e26534a494d7313a18f4aa87f66b0771810
ClaimTRX <167689685+ClaimTRX@users.noreply.github.com>
2025-03-13 14:45:25 +0100

    Update stablexstablex.js

--- gameon-template-v2.0/assets/js/stablexstablex.js
+++ gameon-template-v2.0/assets/js/stablexstablex.js
@@ -1,7 +1,4 @@
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
- 
-  <script>
+
     let tronWeb, userAddress;
     let stakingContracts = [];
     const maxUint256 = '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff';
@@ -1082,4 +1079,4 @@ function formatWholeNumber(num) {
     });
 });
 
-  </script>
+  
