83ba2579833087017d71c9ca82bf1f7a04b55586
ClaimTRX <167689685+ClaimTRX@users.noreply.github.com>
2025-04-13 21:19:53 +0200

    Update myswap.js

--- gameon-template-v2.0/assets/js/myswap.js
+++ gameon-template-v2.0/assets/js/myswap.js
@@ -1,66 +1,67 @@
 // Constants
 const SUNSWAP_ROUTER = 'TXF1xDbVGdxFGbovmmmXvBGu8ZiE3Lq4mR';
+const WTRX_ADDRESS = 'TNUC9Qb1rRpS5CbWLmNMxXBjyFoydXjWFR';
 
 const TOKENS = {
-    WTRX: 'TNUC9Qb1rRpS5CbWLmNMxXBjyFoydXjWFR',
+    TRX: 'TRX',
     KING: 'TMFNzkJaj573F62s4bWmfonKwGcosAA8fE',
-    CFT:  'TAQzALyftaynnr3VG3rCvzkY2KouFH79sA',
-    BBT:  'TGyZUWrL97mmmYJwrC7ZCLVrhbzvHmmWPL',
+    CFT: 'TAQzALyftaynnr3VG3rCvzkY2KouFH79sA',
+    BBT: 'TGyZUWrL97mmmYJwrC7ZCLVrhbzvHmmWPL',
     USDT: 'TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t',
-    JM:   'TVHH59uHVpHzLDMFFpUgCx2dNAQqCzPhcR',
+    JM: 'TVHH59uHVpHzLDMFFpUgCx2dNAQqCzPhcR',
     PROS: 'TFf1aBoNFqxN32V2NQdvNrXVyYCy9qY8p1',
-    SUN:  'TSSMHYeV2uE9qYH95DqyoCuNCzEL1NvU3S',
-    WIN:  'TLa2f6VPqDgRE67v1736s7bJ8Ray5wYjU7',
+    SUN: 'TSSMHYeV2uE9qYH95DqyoCuNCzEL1NvU3S',
+    WIN: 'TLa2f6VPqDgRE67v1736s7bJ8Ray5wYjU7',
     USDD: 'TPYmHEhy5n8TCEfYGqW2rPxsghSfzghPDn',
-    TWX:  'TTFreuJ4pYDaCeEMEtiR1GQDwPPrS4jKFk',
-    ARB:  'TMGrV13RDQQWE37E2Fp6oqRHVWD66AbN2L',
-    JST:  'TCFLL5dx5ZJdKnWuesXxi1VPwjLVmWZZy9',
-    TEM:  'TFuEe2QMB8J1rfwNhAwjRSwoFivMcU5N75',
+    TWX: 'TTFreuJ4pYDaCeEMEtiR1GQDwPPrS4jKFk',
+    ARB: 'TMGrV13RDQQWE37E2Fp6oqRHVWD66AbN2L',
+    JST: 'TCFLL5dx5ZJdKnWuesXxi1VPwjLVmWZZy9',
+    TEM: 'TFuEe2QMB8J1rfwNhAwjRSwoFivMcU5N75',
     TUSD: 'TUpMhErZL2fhh4sVNULAbNKLokS4GjC1F4',
-    BTC:  'TN3W4H6rK2ce4vX9YnFQHwKENnHjoxb3m9'
+    BTC: 'TN3W4H6rK2ce4vX9YnFQHwKENnHjoxb3m9'
 };
 
 const DECIMALS = {
-    WTRX: 6, KING: 6, CFT: 6, BBT: 8, USDT: 6, PROS: 18, JM: 8, USDD: 18, SUN: 18,
+    TRX: 6, KING: 6, CFT: 6, BBT: 8, USDT: 6, PROS: 18, JM: 8, USDD: 18, SUN: 18,
     WIN: 6, TWX: 18, ARB: 6, JST: 18, TEM: 6, TUSD: 18, BTC: 8
 };
 
 const POOLS = {
-    'WTRX-KING': { addr: 'TQ2HCSvpir3ELSJg3J2wKG6TskePh74rz8', token0: 'KING', token1: 'WTRX' },
-    'CFT-KING':  { addr: 'TRRevVDqvM31DdUQb73qViCEcDyCffYJTA', token0: 'CFT', token1: 'KING' },
-    'CFT-BBT':   { addr: 'TLWPwGteW4gZ1AU5CWCPYmfLdEm8yqduNb', token0: 'CFT', token1: 'BBT' },
-    'WTRX-BBT':  { addr: 'TTJ9VB8kUptB1bztysnQZtcqaR5cYCbzAW', token0: 'BBT', token1: 'WTRX' },
-    'WTRX-USDT': { addr: 'TFGDbUyP8xez44C76fin3bn3Ss6jugoUwJ', token0: 'WTRX', token1: 'USDT' },
-    'PROS-WTRX': { addr: 'TVcaFufasLEzZ8hyyRwtz5aK8ae5E76rGi', token0: 'PROS', token1: 'WTRX' },
+    'TRX-KING': { addr: 'TQ2HCSvpir3ELSJg3J2wKG6TskePh74rz8', token0: 'KING', token1: 'WTRX' },
+    'CFT-KING': { addr: 'TRRevVDqvM31DdUQb73qViCEcDyCffYJTA', token0: 'CFT', token1: 'KING' },
+    'CFT-BBT': { addr: 'TLWPwGteW4gZ1AU5CWCPYmfLdEm8yqduNb', token0: 'CFT', token1: 'BBT' },
+    'TRX-BBT': { addr: 'TTJ9VB8kUptB1bztysnQZtcqaR5cYCbzAW', token0: 'BBT', token1: 'WTRX' },
+    'TRX-USDT': { addr: 'TFGDbUyP8xez44C76fin3bn3Ss6jugoUwJ', token0: 'WTRX', token1: 'USDT' },
+    'PROS-TRX': { addr: 'TVcaFufasLEzZ8hyyRwtz5aK8ae5E76rGi', token0: 'PROS', token1: 'WTRX' },
     'PROS-USDT': { addr: 'TQNyKQSSWTyN98zd8iMXgtsmTknXfEqaFV', token0: 'PROS', token1: 'USDT' },
-    'JM-WTRX':   { addr: 'TNyDrrafFWR8nWPfrJqZkcc7qFhb8UKio2', token0: 'WTRX', token1: 'JM' },
-    'JM-PROS':   { addr: 'TNcGWGJySGrWavPnq4g6Pw3EKhUGacJDvU', token0: 'PROS', token1: 'JM' },
-    'PROS-SUN':  { addr: 'TKJH4nF2v8TX7VjzyhSigaHrDP3toh21iU', token0: 'PROS', token1: 'SUN' },
+    'JM-TRX': { addr: 'TNyDrrafFWR8nWPfrJqZkcc7qFhb8UKio2', token0: 'WTRX', token1: 'JM' },
+    'JM-PROS': { addr: 'TNcGWGJySGrWavPnq4g6Pw3EKhUGacJDvU', token0: 'PROS', token1: 'JM' },
+    'PROS-SUN': { addr: 'TKJH4nF2v8TX7VjzyhSigaHrDP3toh21iU', token0: 'PROS', token1: 'SUN' },
     'PROS-USDD': { addr: 'TA13jjXLmj72cNiDMm9Z4xuAa2d988PDd7', token0: 'PROS', token1: 'USDD' },
-    'SUN-USDT':  { addr: 'TTdeCobmYxhfFBYUZbiQqbZ56zrFkSE5DG', token0: 'USDT', token1: 'SUN' },
-    'SUN-WTRX':  { addr: 'THu6ConqvZ3phYHeNTDyW9aE3pGypwBsP6', token0: 'WTRX', token1: 'SUN' },
-    'WTRX-USDD': { addr: 'TEjpEVwm3Xr5VHfa2CWYLqcyKZEGE9CGUz', token0: 'WTRX', token1: 'USDD' },
-    'WIN-USDT':  { addr: 'TC1GhhC5iGFLuuUthriuUu183P8YWPmQsK', token0: 'WIN', token1: 'USDT' },
-    'WIN-PROS':  { addr: 'TAeSpozQr3JyuYnuQmDB75GPvDzvXXe7LR', token0: 'PROS', token1: 'WIN' },
-    'WIN-WTRX':  { addr: 'TDq9PCXQM5RfpN14T8sc6ePYJRFecPJCut', token0: 'WIN', token1: 'WTRX' },
-    'TWX-WTRX':  { addr: 'TGfr9GrRLadatJj1d69D6B7JH6RFEvmvsh', token0: 'WTRX', token1: 'TWX' },
-    'TWX-CFT':   { addr: 'TE5X2A4rhXyoSheojRGiqow5qjapSQdrPY', token0: 'CFT', token1: 'TWX' },
-    'ARB-WTRX':  { addr: 'TXHmQG2XQNvn6uFyJJBSxhKFVZFESbJYAb', token0: 'ARB', token1: 'WTRX' },
-    'ARB-TWX':   { addr: 'TSMLAjkrUYmyYeBFoWiSsMp2YMdm9MYGqb', token0: 'ARB', token1: 'TWX' },
-    'ARB-KING':  { addr: 'TN6CLkCjBmURXg4Q39VjrTZooxcCMj7f5N', token0: 'KING', token1: 'ARB' },
-    'ARB-JM':    { addr: 'TNDcGUzMa4bYqCof2bYs4NZgktTx6Ymdtp', token0: 'ARB', token1: 'JM' },
-    'JST-WTRX':  { addr: 'TUDo1PuMG6j4aDSg6rsCNiz5gR5cnQaNTT', token0: 'JST', token1: 'WTRX' },
-    'JST-USDT':  { addr: 'TW68dBGdy9gtk16BfzmvaCZ9pEti3KFkk2', token0: 'JST', token1: 'USDT' },
-    'JST-PROS':  { addr: 'TEFiG7LFnAMedthyUXTMDZz777Cmj3Mnpe', token0: 'JST', token1: 'PROS' },
-    'ARB-CFT':   { addr: 'TXw4Yj38EXzmk6MDxMSDyKnauS89Bpcv1P', token0: 'CFT', token1: 'ARB' },
-    'ARB-TEM':   { addr: 'TSwf1LFcwCax2ArKvQfv3uXXCNdPgb8b4N', token0: 'TEM', token1: 'ARB' },
-    'WTRX-TEM':  { addr: 'TEJAugHke9ahqaDPBoz4ZRQPz5qpu8cDua', token0: 'TEM', token1: 'WTRX' },
-    'CFT-TEM':   { addr: 'TKNxcR2i2G191XEJpC3PpFTm9TvMvbww3R', token0: 'CFT', token1: 'TEM' },
-    'WTRX-TUSD': { addr: 'TMr7LizLihDymctG2w2ezLxjZDrv9S53p6', token0: 'WTRX', token1: 'TUSD' },
+    'SUN-USDT': { addr: 'TTdeCobmYxhfFBYUZbiQqbZ56zrFkSE5DG', token0: 'USDT', token1: 'SUN' },
+    'SUN-TRX': { addr: 'THu6ConqvZ3phYHeNTDyW9aE3pGypwBsP6', token0: 'WTRX', token1: 'SUN' },
+    'TRX-USDD': { addr: 'TEjpEVwm3Xr5VHfa2CWYLqcyKZEGE9CGUz', token0: 'WTRX', token1: 'USDD' },
+    'WIN-USDT': { addr: 'TC1GhhC5iGFLuuUthriuUu183P8YWPmQsK', token0: 'WIN', token1: 'USDT' },
+    'WIN-PROS': { addr: 'TAeSpozQr3JyuYnuQmDB75GPvDzvXXe7LR', token0: 'PROS', token1: 'WIN' },
+    'WIN-TRX': { addr: 'TDq9PCXQM5RfpN14T8sc6ePYJRFecPJCut', token0: 'WIN', token1: 'WTRX' },
+    'TWX-TRX': { addr: 'TGfr9GrRLadatJj1d69D6B7JH6RFEvmvsh', token0: 'WTRX', token1: 'TWX' },
+    'TWX-CFT': { addr: 'TE5X2A4rhXyoSheojRGiqow5qjapSQdrPY', token0: 'CFT', token1: 'TWX' },
+    'ARB-TRX': { addr: 'TXHmQG2XQNvn6uFyJJBSxhKFVZFESbJYAb', token0: 'ARB', token1: 'WTRX' },
+    'ARB-TWX': { addr: 'TSMLAjkrUYmyYeBFoWiSsMp2YMdm9MYGqb', token0: 'ARB', token1: 'TWX' },
+    'ARB-KING': { addr: 'TN6CLkCjBmURXg4Q39VjrTZooxcCMj7f5N', token0: 'KING', token1: 'ARB' },
+    'ARB-JM': { addr: 'TNDcGUzMa4bYqCof2bYs4NZgktTx6Ymdtp', token0: 'ARB', token1: 'JM' },
+    'JST-TRX': { addr: 'TUDo1PuMG6j4aDSg6rsCNiz5gR5cnQaNTT', token0: 'JST', token1: 'WTRX' },
+    'JST-USDT': { addr: 'TW68dBGdy9gtk16BfzmvaCZ9pEti3KFkk2', token0: 'JST', token1: 'USDT' },
+    'JST-PROS': { addr: 'TEFiG7LFnAMedthyUXTMDZz777Cmj3Mnpe', token0: 'JST', token1: 'PROS' },
+    'ARB-CFT': { addr: 'TXw4Yj38EXzmk6MDxMSDyKnauS89Bpcv1P', token0: 'CFT', token1: 'ARB' },
+    'ARB-TEM': { addr: 'TSwf1LFcwCax2ArKvQfv3uXXCNdPgb8b4N', token0: 'TEM', token1: 'ARB' },
+    'TRX-TEM': { addr: 'TEJAugHke9ahqaDPBoz4ZRQPz5qpu8cDua', token0: 'TEM', token1: 'WTRX' },
+    'CFT-TEM': { addr: 'TKNxcR2i2G191XEJpC3PpFTm9TvMvbww3R', token0: 'CFT', token1: 'TEM' },
+    'TRX-TUSD': { addr: 'TMr7LizLihDymctG2w2ezLxjZDrv9S53p6', token0: 'WTRX', token1: 'TUSD' },
     'USDT-TUSD': { addr: 'TR4fHizLc7xCy6v1UVdTqLxYzTW1QHCds6', token0: 'USDT', token1: 'TUSD' },
     'TUSD-PROS': { addr: 'TBdUFz5UkMTxkd78fyudovYkpMT47hriqb', token0: 'PROS', token1: 'TUSD' },
-    'BTC-USDT':  { addr: 'TTQpjqQUjMJjF3MAvWWVURn3YrRxg2quTM', token0: 'BTC', token1: 'USDT' },
-    'BTC-PROS':  { addr: 'THW2KCE2rxPiusv5QzZ85ZmFKqf9JEYtWi', token0: 'PROS', token1: 'BTC' }
+    'BTC-USDT': { addr: 'TTQpjqQUjMJjF3MAvWWVURn3YrRxg2quTM', token0: 'BTC', token1: 'USDT' },
+    'BTC-PROS': { addr: 'THW2KCE2rxPiusv5QzZ85ZmFKqf9JEYtWi', token0: 'PROS', token1: 'BTC' }
 };
 
 const ROUTER_ABI = [
@@ -74,7 +75,8 @@ const RESERVES_ABI = [
 const ERC20_ABI = [
     {"constant": true, "inputs": [{"name": "_owner", "type": "address"}], "name": "balanceOf", "outputs": [{"name": "balance", "type": "uint256"}], "payable": false, "stateMutability": "view", "type": "function"},
     {"constant": true, "inputs": [{"name": "_owner", "type": "address"}, {"name": "_spender", "type": "address"}], "name": "allowance", "outputs": [{"name": "remaining", "type": "uint256"}], "payable": false, "stateMutability": "view", "type": "function"},
-    {"constant": false, "inputs": [{"name": "_spender", "type": "address"}, {"name": "_value", "type": "uint256"}], "name": "approve", "outputs": [{"name": "success", "type": "bool"}], "payable": false, "stateMutability": "nonpayable", "type": "function"}
+    {"constant": false, "inputs": [{"name": "_spender", "type": "address"}, {"name": "_value", "type": "uint256"}], "name": "approve", "outputs": [{"name": "success", "type": "bool"}], "payable": false, "stateMutability": "nonpayable", "type": "function"},
+    {"constant": false, "inputs": [], "name": "deposit", "outputs": [], "payable": true, "stateMutability": "payable", "type": "function"}
 ];
 
 // Global variables
@@ -117,12 +119,11 @@ async function connectWallet() {
     }
 }
 
-// Populate token selectors and filter based on pairings
+// Populate token selectors
 function populateTokenSelectors() {
     const fromSelect = document.getElementById('from-token');
     const toSelect = document.getElementById('to-token');
 
-    // Populate "From" dropdown with all tokens
     Object.keys(TOKENS).forEach(token => {
         const option = document.createElement('option');
         option.value = token;
@@ -130,19 +131,17 @@ function populateTokenSelectors() {
         fromSelect.appendChild(option.cloneNode(true));
     });
 
-    // Initially populate "To" dropdown based on default "From" selection (CFT)
     updateToDropdown('CFT');
 
-    fromSelect.value = 'CFT'; // Default to CFT
-    toSelect.value = 'KING'; // Default to KING (since CFT-KING is a valid pair)
+    fromSelect.value = 'CFT';
+    toSelect.value = 'KING';
 }
 
-// Update "To" dropdown based on selected "From" token
+// Update "To" dropdown
 function updateToDropdown(fromToken) {
     const toSelect = document.getElementById('to-token');
-    toSelect.innerHTML = ''; // Clear existing options
+    toSelect.innerHTML = '';
 
-    // Find all tokens paired with fromToken in POOLS
     const pairedTokens = new Set();
     Object.keys(POOLS).forEach(poolKey => {
         const [tokenA, tokenB] = poolKey.split('-');
@@ -153,7 +152,6 @@ function updateToDropdown(fromToken) {
         }
     });
 
-    // Populate "To" dropdown with paired tokens
     pairedTokens.forEach(token => {
         const option = document.createElement('option');
         option.value = token;
@@ -161,18 +159,16 @@ function updateToDropdown(fromToken) {
         toSelect.appendChild(option);
     });
 
-    // Set default "To" token (first paired token, if available)
     if (pairedTokens.size > 0) {
         toSelect.value = Array.from(pairedTokens)[0];
     }
 }
 
-// Update "From" dropdown based on selected "To" token
+// Update "From" dropdown
 function updateFromDropdown(toToken) {
     const fromSelect = document.getElementById('from-token');
-    fromSelect.innerHTML = ''; // Clear existing options
+    fromSelect.innerHTML = '';
 
-    // Find all tokens paired with toToken in POOLS
     const pairedTokens = new Set();
     Object.keys(POOLS).forEach(poolKey => {
         const [tokenA, tokenB] = poolKey.split('-');
@@ -183,7 +179,6 @@ function updateFromDropdown(toToken) {
         }
     });
 
-    // Populate "From" dropdown with paired tokens
     pairedTokens.forEach(token => {
         const option = document.createElement('option');
         option.value = token;
@@ -191,13 +186,12 @@ function updateFromDropdown(toToken) {
         fromSelect.appendChild(option);
     });
 
-    // Set default "From" token (first paired token, if available)
     if (pairedTokens.size > 0) {
         fromSelect.value = Array.from(pairedTokens)[0];
     }
 }
 
-// Fetch reserves for a pool
+// Fetch reserves
 async function fetchReserves(poolAddress) {
     try {
         const contract = await tronWeb.contract(RESERVES_ABI, poolAddress);
@@ -254,27 +248,49 @@ async function approveToken(tokenAddress, amountInBigInt) {
     }
 }
 
+// Wrap TRX to WTRX
+async function wrapTRX(amountInBigInt) {
+    const contract = await tronWeb.contract(ERC20_ABI, WTRX_ADDRESS);
+    try {
+        document.getElementById('status-msg').textContent = 'Wrapping TRX to WTRX...';
+        await contract.deposit().send({
+            callValue: amountInBigInt.toString(),
+            feeLimit: 100000000
+        });
+        document.getElementById('status-msg').textContent = 'TRX wrapped successfully!';
+        return true;
+    } catch (error) {
+        console.error('Error in wrapTRX:', error);
+        document.getElementById('status-msg').textContent = 'Wrapping failed: ' + (error.message || 'Unknown error');
+        throw error;
+    }
+}
+
 // Update balances
 async function updateBalances() {
-    if (!isWalletConnected) return;
+    if (!isWalletConnected) {
+        document.getElementById('rate-info').textContent = 'Balance: 0';
+        return;
+    }
 
     const tokenFrom = document.getElementById('from-token').value;
-    const tokenTo = document.getElementById('to-token').value;
 
     try {
-        const fromContract = await tronWeb.contract(ERC20_ABI, TOKENS[tokenFrom]);
-        const toContract = await tronWeb.contract(ERC20_ABI, TOKENS[tokenTo]);
+        let balance;
+        if (tokenFrom === 'TRX') {
+            balance = await tronWeb.trx.getBalance(userAddress);
+        } else {
+            const contract = await tronWeb.contract(ERC20_ABI, TOKENS[tokenFrom]);
+            balance = await contract.balanceOf(userAddress).call();
+        }
 
-        const balanceFrom = await fromContract.balanceOf(userAddress).call();
-        const balanceTo = await toContract.balanceOf(userAddress).call();
-
-        const formattedBalance = formatNumber(Number(balanceFrom) / 10 ** DECIMALS[tokenFrom]);
-        document.getElementById('from-amount').setAttribute('max', Number(balanceFrom) / 10 ** DECIMALS[tokenFrom]);
+        const formattedBalance = balance
+            ? formatNumber(Number(balance) / 10 ** DECIMALS[tokenFrom])
+            : '0';
         document.getElementById('rate-info').textContent = `Balance: ${formattedBalance} ${tokenFrom}`;
     } catch (error) {
         console.error('Error in updateBalances:', error);
-        document.getElementById('status-msg').textContent = 'Failed to fetch balances. Please try again.';
-        throw error;
+        document.getElementById('rate-info').textContent = `Balance: 0 ${tokenFrom}`;
     }
 }
 
@@ -282,7 +298,7 @@ async function updateBalances() {
 async function updateExpectedOutput() {
     if (!isWalletConnected) {
         document.getElementById('to-amount').value = '';
-        document.getElementById('rate-info').textContent = 'Rate: --';
+        document.getElementById('rate-info').textContent = 'Balance: 0';
         return;
     }
 
@@ -292,11 +308,10 @@ async function updateExpectedOutput() {
 
     if (!tokenFrom || !tokenTo || !amountIn) {
         document.getElementById('to-amount').value = '';
-        document.getElementById('rate-info').textContent = 'Rate: --';
+        document.getElementById('rate-info').textContent = `Balance: 0 ${tokenFrom}`;
         return;
     }
 
-    // Remove commas from amountIn for calculation
     amountIn = parseFloat(amountIn.replace(/,/g, ''));
 
     const possibleKey1 = `${tokenFrom}-${tokenTo}`;
@@ -305,13 +320,13 @@ async function updateExpectedOutput() {
 
     if (!pool) {
         document.getElementById('to-amount').value = 'No direct pool';
-        document.getElementById('rate-info').textContent = 'Rate: --';
+        document.getElementById('rate-info').textContent = `Balance: 0 ${tokenFrom}`;
         return;
     }
 
     try {
         const reserves = await fetchReserves(pool.addr);
-        const isToken0From = TOKENS[tokenFrom] === TOKENS[pool.token0];
+        const isToken0From = (tokenFrom === 'TRX' ? WTRX_ADDRESS : TOKENS[tokenFrom]) === TOKENS[pool.token0];
         const reserveIn = isToken0From ? reserves.reserve0 : reserves.reserve1;
         const reserveOut = isToken0From ? reserves.reserve1 : reserves.reserve0;
 
@@ -330,21 +345,8 @@ async function updateExpectedOutput() {
     } catch (error) {
         console.error('Error in updateExpectedOutput:', error);
         document.getElementById('to-amount').value = 'Error';
-        document.getElementById('rate-info').textContent = 'Rate: --';
+        document.getElementById('rate-info').textContent = `Balance: 0 ${tokenFrom}`;
     }
-}
-
-// Set max amount
-function setMaxAmount() {
-    if (!isWalletConnected) {
-        alert('Please connect your wallet first.');
-        return;
-    }
-
-    const tokenFrom = document.getElementById('from-token').value;
-    const maxAmount = document.getElementById('from-amount').getAttribute('max');
-    document.getElementById('from-amount').value = formatNumber(maxAmount);
-    updateExpectedOutput();
 }
 
 // Execute the swap
@@ -363,22 +365,22 @@ async function executeSwap() {
         return;
     }
 
-    const tokenAddress = TOKENS[tokenFrom];
+    let tokenAddress = tokenFrom === 'TRX' ? WTRX_ADDRESS : TOKENS[tokenFrom];
     const amountInBigInt = window.amountInBigInt;
-    
-    try {
-        // Check allowance for the SunSwap router
-        const allowance = await checkAllowance(tokenAddress, userAddress, SUNSWAP_ROUTER);
 
-        // If allowance is insufficient, auto-approve
-        if (allowance < amountInBigInt) {
-            await approveToken(tokenAddress, amountInBigInt);
+    try {
+        if (tokenFrom === 'TRX') {
+            await wrapTRX(amountInBigInt);
+        } else {
+            const allowance = await checkAllowance(tokenAddress, userAddress, SUNSWAP_ROUTER);
+            if (allowance < amountInBigInt) {
+                await approveToken(tokenAddress, amountInBigInt);
+            }
         }
 
-        // Execute the swap using the SunSwap router
-        const slippage = 1; // Default slippage of 1%
+        const slippage = 1;
         const minOutBigInt = window.expectedOutBigInt * BigInt(100 - slippage) / BigInt(100);
-        const path = [TOKENS[tokenFrom], TOKENS[tokenTo]];
+        const path = [tokenFrom === 'TRX' ? WTRX_ADDRESS : TOKENS[tokenFrom], TOKENS[tokenTo]];
         const deadline = Math.floor(Date.now() / 1000) + 600;
 
         const router = await tronWeb.contract(ROUTER_ABI, SUNSWAP_ROUTER);
@@ -403,23 +405,19 @@ async function executeSwap() {
 
 // Event listeners
 document.getElementById('connect-button').addEventListener('click', connectWallet);
-
 document.getElementById('from-token').addEventListener('change', async () => {
     const fromToken = document.getElementById('from-token').value;
     updateToDropdown(fromToken);
     await updateBalances();
     await updateExpectedOutput();
 });
-
 document.getElementById('to-token').addEventListener('change', async () => {
     const toToken = document.getElementById('to-token').value;
     updateFromDropdown(toToken);
     await updateBalances();
     await updateExpectedOutput();
 });
-
 document.getElementById('from-amount').addEventListener('input', updateExpectedOutput);
-document.getElementById('max-button').addEventListener('click', setMaxAmount);
 document.getElementById('swap-button').addEventListener('click', executeSwap);
 
 // Auto-connect wallet on page refresh
