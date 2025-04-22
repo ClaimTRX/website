56e1100a21419d30a48c57855677fd90c76ca7be
ClaimTRX <167689685+ClaimTRX@users.noreply.github.com>
2025-04-22 07:58:36 +0200

    Update swap.js

--- gameon-template-v2.0/assets/js/swap.js
+++ gameon-template-v2.0/assets/js/swap.js
@@ -9,19 +9,10 @@ const TOKENS = {
     STBLX: 'TGd1irpHHU8cFC4ArY9KBoBiocQr1vVpWS', // StableX token
     BBT: 'TGyZUWrL97mmmYJwrC7ZCLVrhbzvHmmWPL',
     KING: 'TMFNzkJaj573F62s4bWmfonKwGcosAA8fE',
-    JM: 'TVHH59uHVpHzLDMFFpUgCx2dNAQqCzPhcR',
+   
     USDD: 'TXDk8mbtRbXeYuMNS83CfKPaYYT8XWv9Hz', // USDD for StableX
-    USDT: 'TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t',
-    USDDOLD: 'TPYmHEhy5n8TCEfYGqW2rPxsghSfzghPDn', // USDD old for pools
-    SUN: 'TSSMHYeV2uE9qYH95DqyoCuNCzEL1NvU3S',
-    TWX: 'TTFreuJ4pYDaCeEMEtiR1GQDwPPrS4jKFk',
-    PROS: 'TFf1aBoNFqxN32V2NQdvNrXVyYCy9qY8p1',
-    WIN: 'TLa2f6VPqDgRE67v1736s7bJ8Ray5wYjU7',
-    ARB: 'TMGrV13RDQQWE37E2Fp6oqRHVWD66AbN2L',
-    JST: 'TCFLL5dx5ZJdKnWuesXxi1VPwjLVmWZZy9',
-    TEM: 'TFuEe2QMB8J1rfwNhAwjRSwoFivMcU5N75', // Corrected TEM address
-    TUSD: 'TUpMhErZL2fhh4sVNULAbNKLokS4GjC1F4',
-    BTC: 'TN3W4H6rK2ce4vX9YnFQHwKENnHjoxb3m9'
+    USDT: 'TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t'
+   
 };
 
 const DECIMALS = {
@@ -30,18 +21,9 @@ const DECIMALS = {
     CFT: 6,
     BBT: 8,
     USDT: 6,
-    PROS: 18,
-    JM: 8,
-    USDDOLD: 18,
+  
     USDD: 18,
-    SUN: 18,
-    WIN: 6,
-    TWX: 18,
-    ARB: 6,
-    JST: 18,
-    TEM: 6,
-    TUSD: 18,
-    BTC: 8,
+    
     STBLX: 6 // 6 decimals for STBLX
 };
 
@@ -50,37 +32,8 @@ const POOLS = {
     'CFT-KING':  { addr: 'TXm1bnUSVSZWE5PpYzzyiZAoQVpFDbNq38', token0: 'CFT', token1: 'KING' },
     'CFT-BBT':   { addr: 'TANnrhzkhjGtgAmD1GQ91rgrmRwKu7sedj', token0: 'BBT', token1: 'CFT' },
     'WTRX-BBT': { addr: 'TTJ9VB8kUptB1bztysnQZtcqaR5cYCbzAW', token0: 'BBT', token1: 'WTRX' },
-    'WTRX-USDT': { addr: 'TFGDbUyP8xez44C76fin3bn3Ss6jugoUwJ', token0: 'WTRX', token1: 'USDT' },
-    'PROS-WTRX': { addr: 'TVcaFufasLEzZ8hyyRwtz5aK8ae5E76rGi', token0: 'PROS', token1: 'WTRX' },
-    'PROS-USDT': { addr: 'TQNyKQSSWTyN98zd8iMXgtsmTknXfEqaFV', token0: 'PROS', token1: 'USDT' },
-    'JM-WTRX': { addr: 'TNyDrrafFWR8nWPfrJqZkcc7qFhb8UKio2', token0: 'WTRX', token1: 'JM' },
-    'JM-PROS': { addr: 'TNcGWGJySGrWavPnq4g6Pw3EKhUGacJDvU', token0: 'PROS', token1: 'JM' },
-    'PROS-SUN': { addr: 'TKJH4nF2v8TX7VjzyhSigaHrDP3toh21iU', token0: 'PROS', token1: 'SUN' },
-    'PROS-USDDOLD': { addr: 'TA13jjXLmj72cNiDMm9Z4xuAa2d988PDd7', token0: 'PROS', token1: 'USDDOLD' },
-    'SUN-USDT': { addr: 'TTdeCobmYxhfFBYUZbiQqbZ56zrFkSE5DG', token0: 'USDT', token1: 'SUN' },
-    'SUN-WTRX': { addr: 'THu6ConqvZ3phYHeNTDyW9aE3pGypwBsP6', token0: 'WTRX', token1: 'SUN' },
-    'WTRX-USDDOLD': { addr: 'TEjpEVwm3Xr5VHfa2CWYLqcyKZEGE9CGUz', token0: 'WTRX', token1: 'USDDOLD' },
-    'WIN-USDT': { addr: 'TC1GhhC5iGFLuuUthriuUu183P8YWPmQsK', token0: 'WIN', token1: 'USDT' },
-    'WIN-PROS': { addr: 'TAeSpozQr3JyuYnuQmDB75GPvDzvXXe7LR', token0: 'PROS', token1: 'WIN' },
-    'WIN-WTRX': { addr: 'TDq9PCXQM5RfpN14T8sc6ePYJBSxhKFVZFESbJYAb', token0: 'WIN', token1: 'WTRX' },
-    'TWX-WTRX': { addr: 'TGfr9GrRLadatJj1d69D6B7JH6RFEvmvsh', token0: 'WTRX', token1: 'TWX' },
-    'TWX-CFT':   { addr: 'TMXAuCPyfMgbvaA6LM4rhSqVs9QinuuCLM', token0: 'CFT', token1: 'TWX' },
-    'ARB-WTRX': { addr: 'TXHmQG2XQNvn6uFyJJBSxhKFVZFESbJYAb', token0: 'ARB', token1: 'WTRX' },
-    'ARB-TWX': { addr: 'TSMLAjkrUYmyYeBFoWiSsMp2YMdm9MYGqb', token0: 'ARB', token1: 'TWX' },
-    'ARB-KING': { addr: 'TN6CLkCjBmURXg4Q39VjrTZooxcCMj7f5N', token0: 'KING', token1: 'ARB' },
-    'ARB-JM': { addr: 'TNDcGUzMa4bYqCof2bYs4NZgktTx6Ymdtp', token0: 'ARB', token1: 'JM' },
-    'JST-WTRX': { addr: 'TUDo1PuMG6j Jefferson4aDSg6rsCNiz5gR5cnQaNTT', token0: 'JST', token1: 'WTRX' },
-    'JST-USDT': { addr: 'TW68dBGdy9gtk16BfzmvaCZ9pEti3KFkk2', token0: 'JST', token1: 'USDT' },
-    'JST-PROS': { addr: 'TEFiG7LFnAMedthyUXTMDZz777Cmj3Mnpe', token0: 'JST', token1: 'PROS' },
-    'ARB-CFT':   { addr: 'TX1mkS1kKNJMcPb4cnGgoxweogSVFeDjq2', token0: 'CFT', token1: 'ARB' },
-    'ARB-TEM': { addr: 'TSwf1LFcwCax2ArKvQfv3uXXCNdPgb8b4N', token0: 'TEM', token1: 'ARB' },
-    'WTRX-TEM': { addr: 'TEJAugHke9ahqaDPBoz4ZRQPz5qpu8cDua', token0: 'TEM', token1: 'WTRX' },
-    'CFT-TEM':   { addr: 'TKbaPV5pAK5xuUdkxeut8EK6EuWqpLJdog', token0: 'TEM', token1: 'CFT' },
-    'WTRX-TUSD': { addr: 'TMr7LizLihDymctG2w2ezLxjZDrv9S53p6', token0: 'WTRX', token1: 'TUSD' },
-    'USDT-TUSD': { addr: 'TR4fHizLc7xCy6v1UVdTqLxYzTW1QHCds6', token0: 'USDT', token1: 'TUSD' },
-    'TUSD-PROS': { addr: 'TBdUFz5UkMTxkd78fyudovYkpMT47hriqb', token0: 'PROS', token1: 'TUSD' },
-    'BTC-USDT': { addr: 'TTQpjqQUjMJjF3MAvWWVURn3YrRxg2quTM', token0: 'BTC', token1: 'USDT' },
-    'BTC-PROS': { addr: 'THW2KCE2rxPiusv5QzZ85ZmFKqf9JEYtWi', token0: 'PROS', token1: 'BTC' }
+    'WTRX-USDT': { addr: 'TFGDbUyP8xez44C76fin3bn3Ss6jugoUwJ', token0: 'WTRX', token1: 'USDT' }
+   
 };
 
 const ROUTER_ABI = [
@@ -336,8 +289,8 @@ function populateTokenSelectors() {
     // Initially populate "To" dropdown based on default "From" selection (CFT)
     updateToDropdown('CFT');
 
-    fromSelect.value = 'CFT'; // Default to CFT
-    toSelect.value = 'KING'; // Default to KING
+    fromSelect.value = 'TRX'; // Default to CFT
+    toSelect.value = 'CFT'; // Default to KING
 }
 
 // Update "To" dropdown based on selected "From" token
