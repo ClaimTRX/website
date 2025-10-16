dc1c9bce45f0f0d8d351e239e5c677fb58cf6334
ClaimTRX <167689685+ClaimTRX@users.noreply.github.com>
2025-10-16 08:49:19 +0200

    Create advertisment.js

---
+++ gameon-template-v2.0/assets/js/advertisment.js
@@ -0,0 +1,45 @@
+const advertisements = [
+  {
+    title: "CFT Guardian Minting is Now Live",
+    description: "Earn daily SOL rewards by staking our Guardian NFTs",
+    image: "assets/img/content/guardian.png",
+    link: "https://cftguardians.com/",
+    linkText: "Mint Now",
+    icon: "icon-rocket"
+  },
+  {
+    title: "Stake StableX",
+    description: "Earn both CFT and StableX with StableX staking",
+    image: "assets/img/content/stablex.png",
+    link: "https://www.cftecosystem.com/buystablex",
+    linkText: "Stake Now",
+    icon: "icon-stake"
+  }
+];
+
+function rotateAdvertisements() {
+  const adContainer = document.querySelector('.luxe-ad-card .row');
+  if (!adContainer) return;
+  let currentAdIndex = 0;
+  const updateAd = () => {
+    const ad = advertisements[currentAdIndex];
+    adContainer.innerHTML = `
+      <div class="col-12 col-md-5 text-center p-3">
+        <img src="${ad.image}" alt="${ad.title}" style="max-width:220px" loading="lazy">
+      </div>
+      <div class="col-12 col-md-7 p-3">
+        <div class="inner">
+          <h2 class="m-0">${ad.title}</h2>
+          <p class="mb-3">${ad.description}</p>
+          <a class="btn primary" href="${ad.link}" aria-label="${ad.linkText}"><i class="${ad.icon} me-2"></i>${ad.linkText}</a>
+        </div>
+      </div>
+    `;
+    currentAdIndex = (currentAdIndex + 1) % advertisements.length;
+  };
+  updateAd();
+  setInterval(updateAd, 60000);
+}
+
+// Initialize ad rotation on DOM content loaded
+document.addEventListener('DOMContentLoaded', rotateAdvertisements);
