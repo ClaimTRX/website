fffbdb84102a4ce2fa36f81c29228c5756cfc22f
ClaimTRX <167689685+ClaimTRX@users.noreply.github.com>
2026-02-24 06:38:41 +0100

    Update poolstakingbtc.js

--- gameon-template-v2.0/assets/js/poolstakingbtc.js
+++ gameon-template-v2.0/assets/js/poolstakingbtc.js
@@ -15,8 +15,8 @@ const ENERGY_RENTAL_DURATION = 5;
 const CACHE_TIMEOUT_MS = 120_000;
 const UI_REFRESH_DELAY_MS = 2000;
 
-const CFT_PRICE_USDT = 0.1623;
-const BTC_PRICE_USDT = 76000;
+const CFT_PRICE_USDT = 0.153;
+const BTC_PRICE_USDT = 65000;
 const CFT_PRICE_BTC = CFT_PRICE_USDT / BTC_PRICE_USDT;
 const DAILY_PAYOUT_PERCENTAGE = 1;
 let energyRentalAvailable = true;   // ← Add this
